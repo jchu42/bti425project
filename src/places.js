ea178be8881b5d551aca07557593b620f7d2ce02
azahra <almasziaya@gmail.com>
2024-04-13 21:11:45 -0400

    Added the rest of the locations and an images folder

--- src/places.js
+++ src/places.js
@@ -12,7 +12,7 @@ export default[
             "longitude": -79.722409
         },
         "Image": "https://www1.brampton.ca/EN/residents/Recreation/Community-Centres/PublishingImages/centre-banners/centre-chingpark.jpg?RenditionID=5",
-        "WebsiteLink": "https://www.brampton.ca/EN/Residents/Recreation/Community-Centres/Chinguacousy-Park/pages/welcome.aspx"
+        "Website link": "https://www.brampton.ca/EN/Residents/Recreation/Community-Centres/Chinguacousy-Park/pages/welcome.aspx"
     },
 
     {
@@ -28,7 +28,7 @@ export default[
             "longitude": -79.463690
         },
         "Image": "https://www.highparktoronto.com/images/parks/home/579.jpg",
-        "WebsiteLink": "https://www.toronto.ca/explore-enjoy/parks-gardens-beaches/high-park/"
+        "Website link": "https://www.toronto.ca/explore-enjoy/parks-gardens-beaches/high-park/"
     },
     {
         "ID": "place-003",
@@ -43,7 +43,7 @@ export default[
             "longitude": -79.513386
         },
         "Image": "https://www.toronto.ca/data/parks/img/466/1.jpg",
-        "WebsiteLink": "https://www.toronto.ca/data/parks/prd/facilities/complex/466/index.html"
+        "Website link": "https://www.toronto.ca/data/parks/prd/facilities/complex/466/index.html"
     },
     {
         "ID": "place-004",
@@ -58,7 +58,7 @@ export default[
             "longitude": -79.79281
         },
         "Image": "https://trcaca.s3.ca-central-1.amazonaws.com/app/uploads/2016/02/17185056/HEART_LAKE_NEW_HERO_IMAGE.jpg",
-        "WebsiteLink": "https://trca.ca/parks/heart-lake-conservation-area/"
+        "Website link": "https://trca.ca/parks/heart-lake-conservation-area/"
     },
     {
         "ID": "place-005",
@@ -73,7 +73,7 @@ export default[
             "longitude": -79.779784
         },
         "Image": "https://www.brampton.ca/EN/residents/Recreation/Community-Centres/PublishingImages/centre-banners/banner-eldorado.jpg?RenditionID=5",
-        "WebsiteLink": "https://www.brampton.ca/EN/residents/Recreation/Community-Centres/Pages/Eldorado-Park.aspx"
+        "Website link": "https://www.brampton.ca/EN/residents/Recreation/Community-Centres/Pages/Eldorado-Park.aspx"
     },
     {
         "ID": "place-006",
@@ -88,7 +88,7 @@ export default[
             "longitude": -79.557951
         },
         "Image": "https://www.mississauga.ca/wp-content/uploads/2021/04/28082429/Promenade_Park_Banner.jpg",
-        "WebsiteLink": "https://www.mississauga.ca/events-and-attractions/parks/lakefront-promenade/"
+        "Website link": "https://www.mississauga.ca/events-and-attractions/parks/lakefront-promenade/"
     },
     {
         "ID": "place-007",
@@ -103,7 +103,7 @@ export default[
             "longitude": -79.642492
         },
         "Image": "https://trcaca.s3.ca-central-1.amazonaws.com/app/uploads/2016/04/17183734/RS8074_TRCA-CAs-JULY-29-2012_037-lpr.jpg",
-        "WebsiteLink": "https://trca.ca/parks/claireville-conservation-area/"
+        "Website link": "https://trca.ca/parks/claireville-conservation-area/"
     },
     {
         "ID": "place-008",
@@ -118,7 +118,7 @@ export default[
             "longitude": -79.873140
         },
         "Image": "https://www.rbg.ca/app/uploads/ServiceBerryTrees-HendriePark-scaled-2200x1467.jpg",
-        "WebsiteLink": "https://www.rbg.ca/"
+        "Website link": "https://www.rbg.ca/"
     },
     {
         "ID": "place-009",
@@ -133,7 +133,7 @@ export default[
             "longitude": -79.231713
         },
         "Image": "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,f_jpg,g_xy_center,h_634,q_65,w_1920,x_2480,y_2284/v1/clients/toronto/167_3_4816_jpeg_fc72e7b7-064e-4690-ab2f-0762667a7712.jpg",
-        "WebsiteLink": "https://www.toronto.ca/explore-enjoy/parks-gardens-beaches/scarborough-bluffs/"
+        "Website link": "https://www.toronto.ca/explore-enjoy/parks-gardens-beaches/scarborough-bluffs/"
     },
     {
         "ID": "place-010",
@@ -148,7 +148,7 @@ export default[
             "longitude": -79.377170
         },
         "Image": "https://www.centreisland.ca/uploads/slideshow/28c7bf2f086bb8da8b544e0d168714c5.jpg",
-        "WebsiteLink": "https://www.centreisland.ca/centreville-amusement-park"
+        "Website link": "https://www.centreisland.ca/centreville-amusement-park"
     },
     {
         "ID": "place-011",
@@ -163,7 +163,7 @@ export default[
             "longitude": -79.055590
         },
         "Image": "https://www.niagaraparks.com/media/2016/11/butterfly-emergence-window.jpg",
-        "WebsiteLink": "https://www.niagaraparks.com/visit/attractions/butterfly-conservatory/"
+        "Website link": "https://www.niagaraparks.com/visit/attractions/butterfly-conservatory/"
     },
     {
         "ID": "place-012",
@@ -178,7 +178,7 @@ export default[
             "longitude": -79.185437
         },
         "Image": "https://www.torontozoo.com/!/img/pg/attractions/kangaroo-walk-thru-423.jpg",
-        "WebsiteLink": "https://www.torontozoo.com/"
+        "Website link": "https://www.torontozoo.com/"
     },
     {
         "ID": "place-013",
@@ -193,7 +193,7 @@ export default[
             "longitude": -79.338926
         },
         "Image": "https://www.ontariosciencecentre.ca/media/1021/building.jpg",
-        "WebsiteLink": "https://www.ontariosciencecentre.ca/"
+        "Website link": "https://www.ontariosciencecentre.ca/"
     },
     {
         "ID": "place-014",
@@ -208,7 +208,7 @@ export default[
             "longitude": -79.394777
         },
         "Image": "https://d3qvqlc701gzhm.cloudfront.net/full/f723d64a8ed4399d41f4fc3d44afa36636124bdfa4f2db24cabefc8743d080e9.jpg",
-        "WebsiteLink": "https://www.rom.on.ca/en"
+        "Website link": "https://www.rom.on.ca/en"
     },
     {
         "ID": "place-015",
@@ -223,6 +223,246 @@ export default[
             "longitude": -79.387061
         },
         "Image": "https://www.juicer.io/api/posts/478051783/images.jpg?external_id=C1j8_kvA79W&s=19433b9f29af00b05f0f14853d11acb6501cf523",
-        "WebsiteLink": "https://www.cntower.ca/"
+        "Website link": "https://www.cntower.ca/"
+    },
+    {
+        "ID": "place-016",
+        "Name": "Ripley's Aquarium of Canada",
+        "Description": "Dive Into an Underwater Adventure. Immerse yourself among thousands of awe-inspiring aquatic animals and get closer than ever before with interactive, hands-on experiences, unique animal encounters, and exciting events!",
+        "Price": 45.00,
+        "Discontinued": false,
+        "Categories": ["cat-03", "cat-07", "cat-09", "cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.642179,
+            "longitude": -79.386602
+        },
+        "Image": "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_xy_center,h_640,q_80,w_640,x_773,y_714/v1/clients/toronto/Ripleys_Aquarium_tunnel_custom_1_1ab7b881-0d9f-4e3a-823d-6dc39ab20880.jpg",
+        "Website link": "https://www.ripleys.com/attractions/ripleys-aquarium-of-canada"
+    },
+    {
+        "ID": "place-017",
+        "Name": "iFly Oakville",
+        "Description": "Soar like in a bird in our 14ft (4.3m) diameter, super-smooth, super-fast and super-cool wind tunnel.  With wheelchair access and highly trained instructors, we welcome flyers of all abilities.",
+        "Price": 79.99,
+        "Discontinued": false,
+        "Categories": ["cat-03","cat-09", "cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.509534,
+            "longitude": 79.666165
+        },
+        "Image": "https://iflyworld.ca/media/nwgjvmhw/01022017-_z8i1656ifly_flight.jpg?anchor=center&rmode=crop&width=1024&height=576.0000",
+        "Website link": "https://iflyworld.ca/locations/ifly-oakville#tab-content--0"
+    },
+    {
+        "ID": "place-018",
+        "Name": "Sky Zone",
+        "Description": "The kind of play that makes us jump and dodge and flip and sweat and laugh and bob and weave until we need to drink some water and lie down.",
+        "Price": 29.99,
+        "Discontinued": false,
+        "Categories": ["cat-01","cat-03", "cat-06", "cat-09", "cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.706524,
+            "longitude": -79.356939
+        },
+        "Image": "https://www.skyzone.com/wp-content/uploads/2023/05/KWP_0669-edit_lr.jpg",
+        "Website link": "https://www.skyzone.com/"
+    },
+    {
+        "ID": "place-019",
+        "Name": "Canada's Wonderland",
+        "Description": "Since 1981, Canada's Wonderland has been the premier destination in Ontario for thrills, family fun, and world-class entertainment. The amusement park features more than 200 attractions including 18 roller coasters, two children's areas, and Splash Works, the 20-acre water park.",
+        "Price": 44.99,
+        "Discontinued": false,
+        "Categories": ["cat-01", "cat-03", "cat-08", "cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.842362,
+            "longitude": -79.541216
+        },
+        "Image": "https://cdn-cloudfront.cfauthx.com/binaries/content/gallery/cw-en-ca/banners/home/banner-large---1440x690--spring2sp24-cw.jpg",
+        "Website link": "https://www.canadaswonderland.com/"
+    },
+    {
+        "ID": "place-020",
+        "Name": "MarineLand",
+        "Description": "Explore the fascinating world of marine mammals including beluga whales, dolphins, and penguins. Enjoy an exhilarating experience with a wide range of rides, including one of the world’s largest steel roller coasters. Refuel your energy at The Hungry Bear restaurant, and take in the stunning open spaces and picnic tables. Marineland® is the perfect destination for a fun-filled family day out, with attractions that appeal to visitors of all ages.",
+        "Price": 52.95,
+        "Discontinued": false,
+        "Categories": ["cat-01", "cat-03", "cat-07", "cat-08"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.065480,
+            "longitude": -79.072434
+        },
+        "Image": "https://www.marineland.ca/storage/2023/04/Feature3Bg.png",
+        "Website link": "https://www.marineland.ca/"
+    },
+    {
+        "ID": "place-021",
+        "Name": "The Falls View, Niagara Falls",
+        "Description": "Witnessing the extraordinary beauty and power of Niagara Falls with your own two eyes is possible 24/7, 365 days a year. Take a leisurely stroll along the promenade, breathe in the mist, and be enchanted by the sheer power of nature's masterpiece or take a long drive along the Niagara Parkway to get a peek from the car, the experience is always free of charge.",
+        "Price": 0,
+        "Discontinued": false,
+        "Categories": ["cat-02", "cat-05", "cat-08", "cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.082086,
+            "longitude": -79.075619
+        },
+        "Image": "https://www.niagarafallstourism.com/site/assets/files/74928/falls_web.jpg",
+        "Website link": "https://www.niagarafallstourism.com/"
+    },
+    {
+        "ID": "place-022",
+        "Name": "Treetop Trekking",
+        "Description": "Located in the natural oasis that is Heart Lake Conservation Area, our Brampton park provides impressive water views and mature forest fun — without ever leaving the city. Pack a picnic, prepare for a bustling atmosphere, and be sure to try the Blue Heron twin ziplines that soar across Heart Lake.",
+        "Price": 67.00,
+        "Discontinued": false,
+        "Categories": ["cat-01", "cat-03", "cat-04","cat-05", "cat-06", "cat-08"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.737737,
+            "longitude": -79.793755
+        },
+        "Image": "images\\treetoptrekking.jpg",
+        "Website link": "https://treetoptrekking.com/parks/brampton"
+    },
+    {
+        "ID": "place-023",
+        "Name": "Art Gallery of Ontario",
+        "Description": "Welcome to the AGO! One of the largest museums in North America, with a collection of more than 120,000 artworks, there is always something new to see and do at the AGO.",
+        "Price": 40.00,
+        "Discontinued": false,
+        "Categories": ["cat-03","cat-09", "cat-09", "cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.653222,
+            "longitude": -79.392303
+        },
+        "Image": "https://ago.ca/sites/default/files/styles/hero_slide_wide/public/2017-12/A-182434.jpg?h=de3d98a1&itok=rXtb95Mt",
+        "Website link": "https://ago.ca/"
+    },
+    {
+        "ID": "place-024",
+        "Name": "The Ontario Place Park",
+        "Description": "Opened on May 22, 1971, and operated as a theme park centered around Ontario themes and family attractions until 2012 when the Government of Ontario announced that it would close for redevelopment. It has since reopened as a park without admission but without several of the old attractions. The Government of Ontario is currently considering further redevelopment of the site.",
+        "Price": 0.00,
+        "Discontinued": true,
+        "Categories": ["cat-01","cat-02","cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.628206,
+            "longitude": -79.415480
+        },
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/8/83/WikiOntarioPlace_Toronto_ID2.jpg",
+        "Website link": "https://ontarioplace.com/en/"
+    },
+    {
+        "ID": "place-025",
+        "Name": "World’s Biggest Bookstore",
+        "Description": "The World's Biggest Bookstore was a bookstore in Toronto, Ontario, Canada, at 20 Edward St, just north of the Toronto Eaton Centre and the Atrium on Bay. Operating from 1980 until 2014, the three-storey store covered 64,000 square feet and was noted for its bright lights and over 20 kilometres of bookshelves.",
+        "Price": 0.00,
+        "Discontinued": true,
+        "Categories": ["cat-02","cat-09","cat-10"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.656994,
+            "longitude": -79.382121
+        },
+        "Image": "https://media.blogto.com/articles/202189-worlds-biggest-bookstore.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70",
+        "Website link": "https://en.wikipedia.org/wiki/World%27s_Biggest_Bookstore"
+    },
+    {
+        "ID": "place-026",
+        "Name": "CF Toronto Eaton Centre",
+        "Description": "CF Toronto Eaton Centre attracts millions of visitors from all over the world and ranks as one of North America’s best performing centres. This unrivaled retail destination is located in the heart of Canada’s most populous city, featuring more than 200 shops including iconic Canadian brands such as Hudson's Bay, Canada Goose, lululemon and Roots, and is the first Canadian shopping centre to feature both Nordstrom and Saks Fifth Avenue.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-10", "cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {   
+            "latitude": 43.654438,
+            "longitude": -79.380699
+        },
+        "Image": "https://assets.cadillacfairview.com/transform/ec0d319b-3654-454b-b480-730bcb120b1c/CF-EATON-CENTRE-JP-0092-1-1?io=transform:fill,width:1600&q=80",
+        "Website link": "https://shops.cadillacfairview.com/property/cf-toronto-eaton-centre"
+    },
+    {
+        "ID": "place-027",
+        "Name": "Bramalea City Centre",
+        "Description": "Bramalea City Centre: Your go-to shopping destination in Brampton, Ontario, boasting a wide array of retail stores and dining options for a complete shopping experience.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {   
+            "latitude": 43.715548,
+            "longitude": -79.721982
+        },
+        "Image": "https://bramaleacitycentre.ca/images/default-popup.jpg?auto=format,compress",
+        "Website link": "https://bramaleacitycentre.ca/"
+    },
+    {
+        "ID": "place-028",
+        "Name": "Yorkdale Shopping Centre",
+        "Description": "Spanned around 2 million square feet and is home to 270 stores, including the country’s largest collection of designer labels and luxury brands and the finest in-home decor and technology. Recognized for its exclusive lineup of first-in-Canada global prestige brands, Yorkdale offers visitors everything they can imagine.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.725435,
+            "longitude": -79.452169
+        },
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/5/58/Yorkdale_Mall.jpg",
+        "Website link": "https://yorkdale.com/"
+    },
+    {
+        "ID": "place-029",
+        "Name": "Pacific Mall",
+        "Description": "Pacific Mall has over 16 events all year around, with over 350 stores & over 2000 parking spaces, we are ready to rock this season, come join us to celebrate.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.826302,
+            "longitude": -79.306259
+        },
+        "Image": "https://lh3.googleusercontent.com/p/AF1QipPT_EWjFA086sKJHawxaJFeVhL2bmlN41k5Ft37=s680-w680-h510",
+        "Website link": "https://www.pacificmalltoronto.ca/"
+    },
+    {
+        "ID": "place-030",
+        "Name": "CF Fairview Mall",
+        "Description": "CF Fairview Mall: Located in Toronto, Ontario, CF Fairview Mall is a premier shopping destination featuring a diverse selection of retail stores, restaurants, and entertainment options. With its modern design and convenient location, it offers a vibrant and dynamic shopping experience for visitors of all ages.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": {
+            "latitude": 43.778657,
+            "longitude": -79.343999
+        },
+        "Image": "https://lh3.googleusercontent.com/p/AF1QipOBHS-uvn8NT8HiQoCocbgVGftz58O16F2a85r2=s680-w680-h510",
+        "Website link": "https://shops.cadillacfairview.com/property/cf-fairview-mall"
+    },
+    {
+        "ID": "place-031",
+        "Name": "Square One Shopping Centre",
+        "Description": "With 330+ stores, restaurants and entertainment concepts, Square One is Ontario’s largest shopping centre, open 362 days a year. Located in Mississauga, its close proximity to Pearson Airport and the Niagara region makes it the perfect shopping & entertainment destination for out-of-town visitors and international travellers.",
+        "Price": 0.00,
+        "Discontinued": false,
+        "Categories": ["cat-09","cat-11"],
+        "Times of Operation": "Dawn to Dusk",
+        "Location": { 
+            "latitude": 43.593001,
+            "longitude": -79.642473
+        },
+        "Image": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/62/38/95/square-one-shopping-centre.jpg?w=1200&h=1200&s=1",
+        "Website link": "https://shopsquareone.com/"
     }
 ]   
