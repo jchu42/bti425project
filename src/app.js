15c594bae228cd7af028b1b0dce023a94495f595
azahra <almasziaya@gmail.com>
2024-04-13 22:09:07 -0400

    Fixed the Places Card and enabled dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,87 +1,113 @@
-import { Card, Form, Alert, Button, Container, Row, Col, Dropdown } from "react-bootstrap";
-import {useAtom} from 'jotai';
-import {dataAtom, errorAtom} from './store.js';
+import {
+  Card,
+  Form,
+  Alert,
+  Button,
+  Container,
+  Row,
+  Col,
+  Dropdown,
+} from "react-bootstrap";
+import { useAtom } from "jotai";
+import { dataAtom, errorAtom } from "./store.js";
 import { useState, useEffect } from "react";
-import Link from 'next/link'
+import Link from "next/link";
+import PlaceCard from "./pages/components/PlacesCard.js";
 
 export default function App() {
-    /*
+  /*
     "All of your store’s dynamic content will be written in JavaScript in the `src/app.js` file. 
     Using JS, you can display the product/service data from a JS array into a proper format on the page (like using table)" 
     */
 
-    const [searchResults, setSearchResults] = useAtom(dataAtom);
-    const [error, setError] = useAtom(errorAtom);
+  const [searchResults, setSearchResults] = useAtom(dataAtom);
+  const [error, setError] = useAtom(errorAtom);
 
-    const CARDS_PER_PAGE = 4;
+  const CARDS_PER_PAGE = 4;
 
-    const [pages, setPages] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [subsetSearchResults, setSubsetSearchResults] = useState([]);
-    
-    useEffect(()=>{
-        if (searchResults && searchResults.length > 0){
-            var pag = [];
-            for(var i = 1; i <= Math.ceil(searchResults.length / CARDS_PER_PAGE); ++i){
-                pag.push(i);
-            }
-            setPages(pag);
+  const [pages, setPages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [subsetSearchResults, setSubsetSearchResults] = useState([]);
 
-            changePage(1); // when search results are initially loaded, set current page to page 1. 
-        }
-    }, [searchResults,]) // reload search results when new search results are given
+  useEffect(() => {
+    if (searchResults && searchResults.length > 0) {
+      var pag = [];
+      for (
+        var i = 1;
+        i <= Math.ceil(searchResults.length / CARDS_PER_PAGE);
+        ++i
+      ) {
+        pag.push(i);
+      }
+      setPages(pag);
 
-    function changePage(pageNumber){ 
-        // could also be useEffect with on pageNumber change, 
-        //  but this allows the function to be called even when the page number doesn't change
-        //  (currentPage 1->1 doesn't trigger useEffect since it doesn't change, 
-        //   resuulting in the search results' page 1 not being reloaded when there's a new search)
-        setCurrentPage(pageNumber);
+      changePage(1); // when search results are initially loaded, set current page to page 1.
+    }
+  }, [searchResults]); // reload search results when new search results are given
 
-        var subsetRes = [];
-        for (var i = (pageNumber - 1)*CARDS_PER_PAGE; i < Math.min((pageNumber) * CARDS_PER_PAGE, searchResults.length); ++i){
-            subsetRes.push(searchResults[i]);
-        }
+  function changePage(pageNumber) {
+    // could also be useEffect with on pageNumber change,
+    //  but this allows the function to be called even when the page number doesn't change
+    //  (currentPage 1->1 doesn't trigger useEffect since it doesn't change,
+    //   resuulting in the search results' page 1 not being reloaded when there's a new search)
+    setCurrentPage(pageNumber);
 
-        setSubsetSearchResults(subsetRes);
+    var subsetRes = [];
+    for (
+      var i = (pageNumber - 1) * CARDS_PER_PAGE;
+      i < Math.min(pageNumber * CARDS_PER_PAGE, searchResults.length);
+      ++i
+    ) {
+      subsetRes.push(searchResults[i]);
     }
 
-    return (
-        <div>
-        {
-            (error || subsetSearchResults.length == 0)
-            ?
-            <>Sorry, it looks like search is not working at this time.</>
-            :
-            <Container><Row>{subsetSearchResults.map((result, index)=>{
-                // https://sentry.io/answers/unique-key-prop/
+    setSubsetSearchResults(subsetRes);
+  }
 
-                return <Col style={{display: "flex", "justifyContent": "space-between", "alignItems": "stretch" }} key={"card" + index}><Card style={{ width: '18rem' }}>
-                    <a href={result["WebsiteLink"]} target="_blank"><Card.Img variant="top" src={result.Image}  width={200} height={200} align="center" style={{ "objectFit": "cover"}}/></a>
-                    <Card.Body>
-                        <Card.Title>{result.Name}</Card.Title>
-                        <Card.Text>
-                            {result.Description} <br />
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        Price: {result.Price == 0?<>Free</>:<>${result.Price.toFixed(2)}</>}
-                        <Link href={"/places/" + result.ID}><Button variant="primary" style={{"float": "right"}}>Go somewhere</Button></Link>
-                    </Card.Footer>
-                </Card></Col>
-            })}</Row></Container>
-        }
+  return (
+    <div>
+      {error || subsetSearchResults.length == 0 ? (
+        <>Sorry, it looks like search is not working at this time.</>
+      ) : (
+        <Container>
+          <Row>
+            {subsetSearchResults.map((result, index) => {
+              // https://sentry.io/answers/unique-key-prop/
+              return (
+                <Col
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "stretch",
+                  }}
+                  key={"card" + index}
+                >
+                    <PlaceCard result={result} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      )}
 
-        {
-            (error || pages.length == 0)
-            ?
-            <></>
-            :
-            <div style={{display: 'flex', justifyContent: 'center'}}>{pages.map((result, index)=>{
-                // return <button key={"pageButton" + index} onClick={()=>changePage(result)}>{result==currentPage?<b><i><u>{result}</u></i></b>:<>{result}</>}</button>
-                return <Button key={"pageButton" + index} variant={result==currentPage?"dark":"outline-dark"} onClick={()=>changePage(result)}>{result}</Button>
-            })}</div>
-        }
+      {error || pages.length == 0 ? (
+        <></>
+      ) : (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {pages.map((result, index) => {
+            // return <button key={"pageButton" + index} onClick={()=>changePage(result)}>{result==currentPage?<b><i><u>{result}</u></i></b>:<>{result}</>}</button>
+            return (
+              <Button
+                key={"pageButton" + index}
+                variant={result == currentPage ? "dark" : "outline-dark"}
+                onClick={() => changePage(result)}
+              >
+                {result}
+              </Button>
+            );
+          })}
         </div>
-    );
+      )}
+    </div>
+  );
 }
