63fbdc1309c94e7785e5b39dff4ed90c230bae07
azahra <almasziaya@gmail.com>
2024-04-13 21:13:53 -0400

    Changed title from Aaaa to Explore Toronto and Enabled StrictMode

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import {useAtom} from 'jotai';
 import styles from '../styles/Home.module.css';
 import App from "../app.js";
+import { StrictMode } from 'react';
 import { useState, useEffect } from "react";
 import {dataAtom, errorAtom} from '../store.js';
 
@@ -11,12 +12,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Aaaaa</title>
+        <title>Explore Toronto</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
-      <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
 
       {/*
       {
