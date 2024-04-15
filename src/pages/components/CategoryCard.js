ca91ad3453aeb4f6fc074079d347a9c456b1f36d
azahra <almasziaya@gmail.com>
2024-04-15 15:15:02 -0400

    Made the Categories card more interactive

--- src/pages/components/CategoryCard.js
+++ src/pages/components/CategoryCard.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import Link from 'next/link';
-import {atom, useAtom} from 'jotai';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import Link from "next/link";
+import { atom, useAtom } from "jotai";
 import { useEffect, useState } from "react";
-import {loggedInAtom, favoritesAtom, historyAtom} from '../../user.js';
-import { dataAtom, errorAtom, searchAtom } from '../../store.js';
+import { loggedInAtom, favoritesAtom, historyAtom } from "../../user.js";
+import { dataAtom, errorAtom, searchAtom } from "../../store.js";
 
-import styles from '../../styles/CategoryCard.module.css';
+import styles from "../../styles/CategoryCard.module.css";
 
 const CategoryCard = ({ categoryInformation }) => {
-    const [searchQuery, setSearchQuery] = useAtom(searchAtom);
+  const [searchQuery, setSearchQuery] = useAtom(searchAtom);
 
   return (
-    <Card className={styles.card}>
+    // <Card className={styles.card}>
+    //   <div className={styles.imageContainer}>
+    //       <img
+    //         src={categoryInformation.Image}
+    //         alt={categoryInformation.description}
+    //         className={styles.image}
+    //       />
+    //     </div>
+    //   <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+    //     <Card.Text><Button onClick={()=>setSearchQuery(categoryInformation.Description)} variant="danger">{categoryInformation.Description}</Button></Card.Text>
+    //   </Card.Body>
+    // </Card>
+    <Card
+      className={styles.card}
+      onClick={() => setSearchQuery(categoryInformation.Description)}
+    >
       <div className={styles.imageContainer}>
-          <img
-            src={categoryInformation.Image}
-            alt={categoryInformation.description}
-            className={styles.image}
-          />
+        <img
+          src={categoryInformation.Image}
+          alt={categoryInformation.description}
+          className={styles.image}
+        />
+        <div className={styles.overlayText}>
+          <p className={styles.overlayTextContent}>
+            {categoryInformation.Description}
+          </p>
         </div>
-      <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-        <Card.Text><Button onClick={()=>setSearchQuery(categoryInformation.Description)} variant="danger">{categoryInformation.Description}</Button></Card.Text>
-      </Card.Body>
+      </div>
     </Card>
   );
 };
