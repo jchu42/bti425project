4246fd6dcd93b2abc23fc2dd4dfa5237b70b5fc6
azahra <almasziaya@gmail.com>
2024-04-15 13:48:07 -0400

    Added images to categories

--- src/categories.js
+++ src/categories.js
@@ -1,50 +1,62 @@
 export default[
     {
         "ID": "cat-00",
-        "Description": "all places"
+        "Description": "All places",
+        "Image": "categories_images\\allplaces.jpg"
     },
     {
         "ID": "cat-01",
-        "Description": "park"
+        "Description": "Parks",
+        "Image": "categories_images\\park.jpg"
     },
     {
         "ID": "cat-02",
-        "Description": "free"
+        "Description": "Free",
+        "Image": "categories_images\\free.jpg"
     },
     {
         "ID": "cat-03",
-        "Description": "paid"
+        "Description": "Paid",
+        "Image": "categories_images\\paid.jpg"
     },
     {
         "ID": "cat-04",
-        "Description": "fishing"
+        "Description": "Fishing",
+        "Image": "categories_images\\fishing.jpg"
     },
     {
         "ID": "cat-05",
-        "Description": "hiking"
+        "Description": "Hiking",
+        "Image": "categories_images\\hiking.jpg"
     },
     {
         "ID": "cat-06",
-        "Description": "exercise"
+        "Description": "Exercise",
+        "Image": "categories_images\\exercise.jpg"
     },
     {
         "ID": "cat-07",
-        "Description": "animals"
+        "Description": "Animals",
+        "Image": "categories_images\\animal.jpg"
     },
     {
         "ID": "cat-08",
-        "Description": "outdoors"
+        "Description": "Outdoors",
+        "Image": "categories_images\\outdoor.jpg"
     },
     {
         "ID": "cat-09",
-        "Description": "indoors"
+        "Description": "Indoors",
+        "Image": "categories_images\\indoor.jpg"
     },
     {
         "ID": "cat-10",
-        "Description": "downtown"
+        "Description": "Downtown",
+        "Image": "categories_images\\downtown.jpg"
     },
     {
         "ID": "cat-11",
-        "Description": "shopping"
+        "Description": "Shopping",
+        "Image": "categories_images\\shopping.jpg"
     }
 ]
--- src/pages/components/CategoryCard.js
+++ src/pages/components/CategoryCard.js
@@ -6,45 +6,23 @@ import { useEffect, useState } from "react";
 import {loggedInAtom, favoritesAtom, historyAtom} from '../../user.js';
 import { dataAtom, errorAtom, searchAtom } from '../../store.js';
 
+import styles from '../../styles/CategoryCard.module.css';
+
 const CategoryCard = ({ categoryInformation }) => {
     const [searchQuery, setSearchQuery] = useAtom(searchAtom);
 
   return (
-    <Card style={{ width: '18rem' }}>
-      {/* <a href={result.WebsiteLink} target="_blank">
-        <Card.Img
-          variant="top"
-          src={result.Image}
-          width={200}
-          height={200}
-          align="center"
-          style={{ objectFit: 'cover' }}
-        />
-      </a> */}
-      <Card.Body>
-        {/* <Card.Title>{result.Name}</Card.Title> */}
+    <Card className={styles.card}>
+      <div className={styles.imageContainer}>
+          <img
+            src={categoryInformation.Image}
+            alt={categoryInformation.description}
+            className={styles.image}
+          />
+        </div>
+      <Card.Body className="d-flex flex-column justify-content-center align-items-center">
         <Card.Text><Button onClick={()=>setSearchQuery(categoryInformation.Description)} variant="danger">{categoryInformation.Description}</Button></Card.Text>
       </Card.Body>
-      {/* <Card.Footer>
-        Price: {result.Price === 0 ? <>Free</> : <>${result.Price.toFixed(2)}</>}
-        {
-          focused?
-          <> 
-            <Button onClick={toggleFavorites} style={{ float: 'right' }}>
-              {
-                favorites.includes(result.ID)?
-                "Remove":"Add"
-              }
-            </Button>
-          </>
-          :
-          <Link href={`/places/${result.ID}`}>
-              <Button variant="btn btn-danger" style={{ float: 'right' }}>
-                Go Here
-              </Button>
-          </Link>
-        }
-      </Card.Footer> */}
     </Card>
   );
 };
