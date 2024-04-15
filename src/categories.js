ee864cbb2b18421ff28b04e65722d5f701caf6d3
azahra <almasziaya@gmail.com>
2024-04-15 14:15:23 -0400

    categories fixed

--- src/categories.js
+++ src/categories.js
@@ -1,62 +1,62 @@
 export default[
     {
         "ID": "cat-00",
-        "Description": "All places",
+        "Description": "all places",
         "Image": "categories_images\\allplaces.jpg"
     },
     {
         "ID": "cat-01",
-        "Description": "Parks",
+        "Description": "parks",
         "Image": "categories_images\\park.jpg"
     },
     {
         "ID": "cat-02",
-        "Description": "Free",
+        "Description": "free",
         "Image": "categories_images\\free.jpg"
     },
     {
         "ID": "cat-03",
-        "Description": "Paid",
+        "Description": "paid",
         "Image": "categories_images\\paid.jpg"
     },
     {
         "ID": "cat-04",
-        "Description": "Fishing",
+        "Description": "fishing",
         "Image": "categories_images\\fishing.jpg"
     },
     {
         "ID": "cat-05",
-        "Description": "Hiking",
+        "Description": "hiking",
         "Image": "categories_images\\hiking.jpg"
     },
     {
         "ID": "cat-06",
-        "Description": "Exercise",
+        "Description": "exercise",
         "Image": "categories_images\\exercise.jpg"
     },
     {
         "ID": "cat-07",
-        "Description": "Animals",
+        "Description": "animals",
         "Image": "categories_images\\animal.jpg"
     },
     {
         "ID": "cat-08",
-        "Description": "Outdoors",
+        "Description": "outdoors",
         "Image": "categories_images\\outdoor.jpg"
     },
     {
         "ID": "cat-09",
-        "Description": "Indoors",
+        "Description": "indoors",
         "Image": "categories_images\\indoor.jpg"
     },
     {
         "ID": "cat-10",
-        "Description": "Downtown",
+        "Description": "downtown",
         "Image": "categories_images\\downtown.jpg"
     },
     {
         "ID": "cat-11",
-        "Description": "Shopping",
+        "Description": "shopping",
         "Image": "categories_images\\shopping.jpg"
     }
 ]
